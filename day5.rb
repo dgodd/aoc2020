202d9bb24f32e3bd7f04d40ff82c89422b3c9f18
Dave Goddard <dave@goddard.id.au>
2020-12-05 16:17:29 +1100

    Day 5 - Part 2 - Answer: 569

--- day5.rb
+++ day5.rb
@@ -14,9 +14,14 @@ puts "SEAT ID: BFFFBBFRRR -> 567 == #{seat_id('BFFFBBFRRR')}"
 puts "SEAT ID: FFFBBBFRRR -> 119 == #{seat_id('FFFBBBFRRR')}"
 puts "SEAT ID: BBFFBBFRLL -> 820 == #{seat_id('BBFFBBFRLL')}"
 
-max_id = File.read('data/day5.txt').split("\n").map do |line|
-  id = seat_id(line)
-  puts id
-  id
-end.max
-puts "MAX: #{max_id}"
+all_ids = File.read('data/day5.txt').split("\n").map do |line|
+  seat_id(line)
+end
+puts "MAX: #{all_ids.max}"
+
+missing = (0..955).to_a - all_ids.sort
+([-1] + missing + [-1]).each_cons(3).each do |(before,val,after)|
+  next if (before + 1) == val || (val + 1) == after
+
+  puts "MY SEAT: #{val}"
+end
