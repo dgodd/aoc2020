c777311fb74ae76bbdf41e4ac58b9bf7b545a612
Dave Goddard <dave@goddard.id.au>
2020-12-07 08:29:58 +1100

    Day 6 - Part 2 - Ruby

--- day6.rb
+++ day6.rb
@@ -20,9 +20,21 @@ end
 def sum_questions(groups)
   groups.sum { |a| a.length }
 end
+def group_all(groups)
+  groups.map do |group|
+    values = group.flat_map { |w| w.split('') }.group_by { |i| i }.values
+    values.filter { |v| v.length == group.length }
+  end
+end
 
 groups = group_uniq(get_groups('data/day6_sample.txt'))
 puts "Day 6: Part 1: Sample: #{sum_questions(groups)}"
 
 groups = group_uniq(get_groups('data/day6.txt'))
 puts "Day 6: Part 1: #{sum_questions(groups)}"
+
+groups = group_all(get_groups('data/day6_sample.txt'))
+puts "Day 6: Part 2: Sample: #{sum_questions(groups)}"
+
+groups = group_all(get_groups('data/day6.txt'))
+puts "Day 6: Part 2: #{sum_questions(groups)}"
