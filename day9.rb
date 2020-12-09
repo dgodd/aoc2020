43742685cccde4215aafccdeb3b662893fce9857
Dave Goddard <dave@goddard.id.au>
2020-12-09 20:10:16 +1100

    Day 9 - Part 2

--- day9.rb
+++ day9.rb
@@ -18,10 +18,34 @@ def first_invalid(numbers, preamble)
   end
 end
 
+def contiguous_sum(numbers, num)
+  numbers.each.with_index do |x, i|
+    sum = x
+    numbers[(i+1)..].each.with_index(i + 1) do |y, j|
+      sum += y
+      break if sum > num
+      return numbers[i..j] if sum == num
+    end
+  end
+end
+
+def sum_min_max(numbers)
+  numbers.min + numbers.max
+end
+
 puts "Part 1 - Sample"
 numbers = File.read('data/day9_sample.txt').split(/\n/).map(&:to_i)
-p first_invalid(numbers, 5)
+p first_invalid(numbers, 5) # 127
 
-puts "Part 1"
+puts "\nPart 1"
 numbers = File.read('data/day9.txt').split(/\n/).map(&:to_i)
-p first_invalid(numbers, 25)
+p first_invalid(numbers, 25) # 21806024
+
+puts "\nPart 2 - Sample"
+numbers = File.read('data/day9_sample.txt').split(/\n/).map(&:to_i)
+p contiguous_sum(numbers, 127)
+p sum_min_max(contiguous_sum(numbers, 127))
+
+puts "\nPart 2"
+numbers = File.read('data/day9.txt').split(/\n/).map(&:to_i)
+p sum_min_max(contiguous_sum(numbers, 21806024))
