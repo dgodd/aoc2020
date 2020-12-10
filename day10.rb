4a1fead9a0866c2dad3e745d6309e14a43594e4c
Dave Goddard <dave@goddard.id.au>
2020-12-10 16:39:21 +1100

    Day10 - Part 2 - WIP

--- day10.rb
+++ day10.rb
@@ -1,26 +1,41 @@
 #!/usr/bin/ruby
 require 'pry'
 
-numbers = File.read('data/day10_sample.txt').split(/\n/).map(&:to_i).sort
-numbers << (numbers[-1] + 3)
-numbers << 0
-numbers.sort!
+def numbers(path)
+  nums = File.read(path).split(/\n/).map(&:to_i).sort
+  nums << (nums[-1] + 3)
+  nums << 0
+  nums.sort!
+end
+
 diff = Hash.new(0)
-numbers.each_cons(2).each do |(a, b)|
-  p [a, b]
+numbers('data/day10_sample.txt').each_cons(2).each do |(a, b)|
+  #p [a, b]
   diff[b - a] += 1
 end
-p diff
+#p diff
 puts "Part 1 - Sample: #{diff[1] * diff[3]}"
 
-numbers = File.read('data/day10.txt').split(/\n/).map(&:to_i).sort
-numbers << (numbers[-1] + 3)
-numbers << 0
-numbers.sort!
-diff = Hash.new(0)
-numbers.each_cons(2).each do |(a, b)|
-  p [a, b]
+numbers('data/day10.txt').each_cons(2).each do |(a, b)|
+  #p [a, b]
   diff[b - a] += 1
 end
-p diff
+# p diff
 puts "Part 1: #{diff[1] * diff[3]}"
+
+def recurse_max3(nums, prev = 0, path = [])
+  # puts "CALL: #{path.inspect}, #{nums.inspect}"
+  # puts "PATH: #{path.inspect}" if nums.length <= 1
+  return 1 if nums.length <= 1
+  sum = 0
+  nums.each.with_index(1) do |n, index|
+    break if n > (prev + 3)
+    rest = nums[index..]
+    # p [n, nums, index, rest]
+    sum += recurse_max3(rest, n, path+[n])
+  end
+  sum
+end
+puts "Part 2 - Sample: #{recurse_max3(numbers('data/day10_sample_small.txt')[1..])}"
+puts "Part 2 - Sample: #{recurse_max3(numbers('data/day10_sample.txt')[1..])}"
+puts "Part 2: #{recurse_max3(numbers('data/day10.txt')[1..])}"
